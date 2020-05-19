4dd5d1b149c9d8428aeb0ab212317f2a051e7f05
IVORY_C <767478058@qq.com>
2020-05-19 16:19:20 +0800

    添加注释

--- rentomatic/use_cases/request_objects.py
+++ rentomatic/use_cases/request_objects.py
@@ -1,15 +1,15 @@
 import collections
 
 from rentomatic.shared import request_object as req
-
+from typing import Dict, Union, Optional
 
 class StorageRoomListRequestObject(req.ValidRequestObject):
 
-    def __init__(self, filters=None):
+    def __init__(self, filters: Optional[str] = None):
         self.filters = filters
 
     @classmethod
-    def from_dict(cls, adict):
+    def from_dict(cls, adict : Dict[str, str]) -> Union["req.InvalidRequestObject", "StorageRoomListRequestObject"]: 
         invalid_req = req.InvalidRequestObject()
 
         if 'filters' in adict and not isinstance(adict['filters'], collections.Mapping):
@@ -18,4 +18,4 @@ class StorageRoomListRequestObject(req.ValidRequestObject):
         if invalid_req.has_errors():
             return invalid_req
 
-        return StorageRoomListRequestObject(filters=adict.get('filters', None))
+        return StorageRoomListRequestObject(filters=adict.get('filters', ""))
--- rentomatic/use_cases/storageroom_use_cases.py
+++ rentomatic/use_cases/storageroom_use_cases.py
@@ -1,12 +1,14 @@
 from rentomatic.shared import use_case as uc
 from rentomatic.shared import response_object as res
+from rentomatic.shared import interface as itf
+
 
 
 class StorageRoomListUseCase(uc.UseCase):
 
-    def __init__(self, repo):
+    def __init__(self, repo : itf.StorageRepos):
         self.repo = repo
 
-    def process_request(self, request_object):
+    def process_request(self, request_object) -> res.ResponseSuccess: 
         domain_storageroom = self.repo.list(filters=request_object.filters)
         return res.ResponseSuccess(domain_storageroom)
