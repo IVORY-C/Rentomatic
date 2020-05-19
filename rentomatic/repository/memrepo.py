b133331530e8a29dbba40015f4aa5fa333d3e44c
IVORY_C <767478058@qq.com>
2020-05-19 16:06:34 +0800

    添加类型注释

--- rentomatic/repository/memrepo.py
+++ rentomatic/repository/memrepo.py
@@ -1,14 +1,17 @@
 from rentomatic.domain import storageroom as sr
+from rentomatic.shared import interface as itf
+from typing import Dict, List, Union, Optional, Any, Iterable
+
 
 
 class MemRepo:
 
-    def __init__(self, entries=None):
+    def __init__(self, entries: List[str] = None):
         self._entries = []
         if entries:
             self._entries.extend(entries)
 
-    def _check(self, element, key, value):
+    def _check(self, element, key: str, value: Any) -> bool:
         if '__' not in key:
             key = key + '__eq'
 
@@ -26,14 +29,14 @@ class MemRepo:
 
         return getattr(element[key], operator)(value)
 
-    def list(self, filters=None):
+    def list(self, filters: Optional[dict] = None) -> List[sr.StorageRoom]:
         if not filters:
             result = self._entries
         else:
-            result = []
+            result = [] 
             result.extend(self._entries)
 
             for key, value in filters.items():
                 result = [e for e in result if self._check(e, key, value)]
 
-        return [sr.StorageRoom.from_dict(r) for r in result]
+        return [sr.StorageRoom.from_dict(r) for r in result] 
